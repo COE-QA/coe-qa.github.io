a25eb14ef527f4a63181399055154dc0ff7f400d
Jaguar <jaguar.arx@gmail.com>
2020-01-31 20:31:59 +0200

    add documents for .net

---
+++ src/components/dotnetdoc.js
@@ -0,0 +1,9 @@
+﻿module.exports = {
+    template: require('./dotnetdoc.html'),
+	beforeRouteEnter(to, from, next) {
+		next(vm => {
+            vm.$root.title = 'COE Test Automation Framework (dotNet) Documentation';
+			jQuery('html').addClass('allwidth');
+	    })
+    }
+}
